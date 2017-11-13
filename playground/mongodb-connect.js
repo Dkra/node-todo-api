d580a5cc0fe03b5c9dc76426269812761d0862e8
Roger Jiang <dkra4741@gmail.com>
2017-11-13 22:03:57 +0800

    add mongo-connection.js

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,36 @@
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+	if (err) {
+		return console.log('Unable to connect to Mongo Server!')
+	}
+
+	console.log('Connected to MongoDB server!')
+
+	db.collection('Todos').insertOne({
+		text: 'Something',
+		completed: false
+	},
+	(err, result) => {
+		if (err) {
+			return console.log('Unable to insert Todo', err)
+		}
+
+		console.log(JSON.stringify(result.ops, undefined, 2))
+	})
+
+	db.collection('User').insertOne({
+		name: 'Roger',
+		age: 28,
+		location: 'Taiwan'
+	},
+	(err, result) => {
+		if (err) {
+			return console.log('Unable to insert User', err)
+		}
+
+		console.log(JSON.stringify(result.ops, undefined, 2))
+	})
+
+	db.close()
+})
